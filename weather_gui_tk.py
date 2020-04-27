1b05788021375ee5f662cf9a09d82662321abc4d
MichalBogoryja <m.michal.bz@gmail.com>
2020-04-27 18:01:40 +0200

    Add weather icons

--- weather_gui_tk.py
+++ weather_gui_tk.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from weather_core import get_location_id, get_weather, get_weather_states_icons, analyse_weather, present_weather_daily
+from PIL import ImageTk,Image
+from weather_core import get_location_id, get_weather, get_weather_states_icons, present_weather_daily, analyse_weather
 
 result_label = []
+img = []
 
 
 def present_weather(data):
@@ -15,20 +17,35 @@ def present_weather(data):
         detailed = True
     else:
         detailed = False
-    for i in range(forecast_range):
-        text = present_weather_daily(weather_data, i, detailed)
+    for day in range(forecast_range):
+        text = present_weather_daily(weather_data, day, detailed)
+        print(text)
         result_label.append(tk.Label(master=window, text=text))
-        result_label[i].grid(row=0, column=i+1)
+        result_label[day].grid(row=1, column=day+1)
+        show_image(day, weather_data)
     btn_no_details.select()
 
     return result_label
 
 
 def clear():
-    global result_label
+    global result_label, img
     for i in range(len(result_label)):
         result_label[i].destroy()
     result_label = []
+    img = []
+
+
+def show_image(col, data):
+    global img
+    weather_data = analyse_weather(data, col)
+    weather_state = weather_data.state_abbr
+    get_weather_states_icons(weather_state)
+    canvas = tk.Canvas(master=window, width=100, height=80, bg="#ADD8E6")
+    canvas.grid(row=0, column=col+1, stick="nsew")
+    img.append(tk.PhotoImage(file=f'{weather_state}.jpg'))
+    canvas.create_image(64, 45, image=img[col])
+    pass
 
 
 window = tk.Tk()
@@ -52,13 +69,6 @@ for i, lab in enumerate(labels):
     entry[lab] = (tk.Entry(master=frame, bg="white", fg="blue"))
     entry[lab].grid(row=i, column=1, sticky="ew")
 
-# print(type(label))
-# print(label)
-# print(label[0])
-# label[0].destroy()
-# print(label)
-
-
 frm_buttons = tk.Frame(master=frame)
 frm_buttons.grid(row=(len(labels)), column=0)
 
@@ -70,10 +80,8 @@ btn_details.pack(side=tk.LEFT, padx=5, ipadx=5)
 btn_no_details = tk.Radiobutton(master=frm_buttons, text='No details', variable=details, value=2)
 btn_no_details.pack(side=tk.LEFT, padx=5, ipadx=5)
 
-btn_submit = tk.Button(master=frame, text="Show forecast", command=lambda: (result_label, present_weather(entry)))
+btn_submit = tk.Button(master=frame, text="Show forecast", command=lambda: present_weather(entry))
 print(btn_submit)
 btn_submit.grid(row=(len(labels)), column=1)
-
-# result_label = present_weather(entry)
 
 window.mainloop()
